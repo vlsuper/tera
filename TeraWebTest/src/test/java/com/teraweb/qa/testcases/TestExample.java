3d3d0d2c4b4085b7b2053c118722a7499f750716
vong.le <vong.le@BTMVNWKS000080.ad.btmglobal.com>
2018-07-20 18:09:30 +0700

    Version 2

---
+++ TeraWebTest/src/test/java/com/teraweb/qa/testcases/TestExample.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.teraweb.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+/**
+ * @author vong.le
+ *
+ */
+public class TestExample {
+
+	@Test
+	public void testA() {
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void testB() {
+		Assert.assertTrue(true);
+		
+	}
+	
+}
